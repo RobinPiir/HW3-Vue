acea52b990c9d6893344570133174ce8fc106938
Yasuven <147263850+Yasuven@users.noreply.github.com>
2023-11-27 16:59:21 +0200

    Header and Footer
    Created Header and Footer

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <h1>Header</h1>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </template>
+
+
+<script>
+export default {
+    name: 'WebHeader',
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
+    <WebHeader/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WebFooter/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import WebFooter from '@/components/footer.vue'
+import WebHeader from '@/components/header.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    WebFooter,
+    WebHeader
   }
 }
 </script>
