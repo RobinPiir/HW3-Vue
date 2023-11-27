9a8ef76ab929f5ede19ac348394943eb807d9c31
Karolin Kerro <kerrokarolin@gmail.com>
2023-11-27 17:44:19 +0200

    Fixed error

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,38 +1,18 @@
 <template>
-        <div class="center">
-        <bold>
-        <p>WELCOME TO POST IT<br></p>
-        </bold>
-        <p id="Sign Up">Create an account<br></p>
-        <bold2>
-        <p>or<br>Sign up here:</p>
-        </bold2>
-    <form action="index.html">
-        <input type="email" name="email" placeholder="Email" required>
-
-        <input type="password" name="password" placeholder="Password" required>
-
-        <a href="index.html">
-            <button type="submit">Sign Up</button><br>
-        </a>
-
-    </form>
-
-        <p>Forgot password<br></p>
-
-    </div>
-<template>
+  <HeaderCompo/>
+    <FooterCompo/>
+</template>
 
 <script>
 // @ is an alias to /src
-import WebFooter from '@/components/footer.vue'
-import WebHeader from '@/components/header.vue'
+import FooterCompo from '@/components/FooterCompo.vue'
+import HeaderCompo from '@/components/HeaderCompo.vue'
 
 export default {
-  name: 'SignUpView',
+  name: 'HomeView',
   components: {
-    WebFooter,
-    WebHeader
+    FooterCompo,
+    HeaderCompo,
   }
 }
 </script>
